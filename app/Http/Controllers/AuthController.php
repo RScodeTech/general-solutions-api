75307251aae8ba296bbe94ef5d02f8937b16154e
sergiojt <50329431+sergiojt@users.noreply.github.com>
2025-08-03 16:34:26 -0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends Controller
             $user->name = (string) $request->name;
             $user->email = (string) $request->email;
             $user->google_id = $request->id;
-            $user->imagem = $request->photo;
+            $user->image = $request->photo;
 
             $user->save();
 
